6a60bf4d4e26ca50dde92b5b2c59eb01efd0192b
PraghadeeshLife <102030901+PraghadeeshLife@users.noreply.github.com>
2022-03-21 18:22:24 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,183 @@
+import java.util.Objects;
+
+class Main {
+  public static void main(String[] args) {
+    int n = 4;
+    Complex[] xinput = new Complex[n];
+
+    for (int j = 0; j < n; j++) {
+        xinput[j] = new Complex(j, 0);
+    }
+    
+    show(fft(xinput),"FFT");
+  }
+
+  public static void show(Complex[] x, String title) {
+        System.out.println(title);
+        System.out.println("-------------------");
+        for (int i = 0; i < x.length; i++) {
+            System.out.println(x[i]);
+        }
+        System.out.println();
+    }
+
+
+  public static Complex[] fft(Complex[] x){
+    int size = x.length;
+    System.out.println(size);
+    if (size == 1){
+      return new Complex[] {x[0]};
+    }
+    if (size%2 != 0){
+      throw new IllegalArgumentException("Size is not a power of two");
+    }
+    Complex[] even = new Complex[size/2];
+    for (int k = 0; k < size/2; k++) {
+            even[k] = x[2*k];
+    }
+    Complex[] evenFFT = fft(even);
+    
+    Complex[] odd = new Complex[size/2];
+    for (int k = 0; k < size/2; k++) {
+            odd[k] = x[2*k+1];
+    }
+    Complex[] oddFFT = fft(odd);
+
+    Complex[] fftOutput = new Complex[size];
+    for(int i=0; i < size/2; i++){
+     double kth = -2 * i * Math.PI / size;
+            Complex wk = new Complex(Math.cos(kth), Math.sin(kth));
+            fftOutput[i]       = evenFFT[i].plus (wk.times(oddFFT[i]));
+            fftOutput[i + size/2] = evenFFT[i].minus(wk.times(oddFFT[i]));
+    }
+
+    return fftOutput;
+  }
+}
+
+
+class Complex {
+    private final double re;   // the real part
+    private final double im;   // the imaginary part
+
+    // create a new object with the given real and imaginary parts
+    public Complex(double real, double imag) {
+        re = real;
+        im = imag;
+    }
+
+    // return a string representation of the invoking Complex object
+    public String toString() {
+        if (im == 0) return re + "";
+        if (re == 0) return im + "i";
+        if (im <  0) return re + " - " + (-im) + "i";
+        return re + " + " + im + "i";
+    }
+
+    // return abs/modulus/magnitude
+    public double abs() {
+        return Math.hypot(re, im);
+    }
+
+    // return angle/phase/argument, normalized to be between -pi and pi
+    public double phase() {
+        return Math.atan2(im, re);
+    }
+
+    // return a new Complex object whose value is (this + b)
+    public Complex plus(Complex b) {
+        Complex a = this;             // invoking object
+        double real = a.re + b.re;
+        double imag = a.im + b.im;
+        return new Complex(real, imag);
+    }
+
+    // return a new Complex object whose value is (this - b)
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double real = a.re - b.re;
+        double imag = a.im - b.im;
+        return new Complex(real, imag);
+    }
+
+    // return a new Complex object whose value is (this * b)
+    public Complex times(Complex b) {
+        Complex a = this;
+        double real = a.re * b.re - a.im * b.im;
+        double imag = a.re * b.im + a.im * b.re;
+        return new Complex(real, imag);
+    }
+
+    // return a new object whose value is (this * alpha)
+    public Complex scale(double alpha) {
+        return new Complex(alpha * re, alpha * im);
+    }
+
+    // return a new Complex object whose value is the conjugate of this
+    public Complex conjugate() {
+        return new Complex(re, -im);
+    }
+
+    // return a new Complex object whose value is the reciprocal of this
+    public Complex reciprocal() {
+        double scale = re*re + im*im;
+        return new Complex(re / scale, -im / scale);
+    }
+
+    // return the real or imaginary part
+    public double re() { return re; }
+    public double im() { return im; }
+
+    // return a / b
+    public Complex divides(Complex b) {
+        Complex a = this;
+        return a.times(b.reciprocal());
+    }
+
+    // return a new Complex object whose value is the complex exponential of this
+    public Complex exp() {
+        return new Complex(Math.exp(re) * Math.cos(im), Math.exp(re) * Math.sin(im));
+    }
+
+    // return a new Complex object whose value is the complex sine of this
+    public Complex sin() {
+        return new Complex(Math.sin(re) * Math.cosh(im), Math.cos(re) * Math.sinh(im));
+    }
+
+    // return a new Complex object whose value is the complex cosine of this
+    public Complex cos() {
+        return new Complex(Math.cos(re) * Math.cosh(im), -Math.sin(re) * Math.sinh(im));
+    }
+
+    // return a new Complex object whose value is the complex tangent of this
+    public Complex tan() {
+        return sin().divides(cos());
+    }
+    
+
+
+    // a static version of plus
+    public static Complex plus(Complex a, Complex b) {
+        double real = a.re + b.re;
+        double imag = a.im + b.im;
+        Complex sum = new Complex(real, imag);
+        return sum;
+    }
+
+    // See Section 3.3.
+    public boolean equals(Object x) {
+        if (x == null) return false;
+        if (this.getClass() != x.getClass()) return false;
+        Complex that = (Complex) x;
+        return (this.re == that.re) && (this.im == that.im);
+    }
+
+    // See Section 3.3.
+    public int hashCode() {
+        return Objects.hash(re, im);
+    }
+
+   
+
+}
+
